1a440db2baf0633f1c4870af1089c6e2e864873e
MosesMuiru <mosesmuiru9@gmail.com>
2023-11-04 22:25:15 +0300

    tests

--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -1,13 +1,16 @@
 defmodule TodoListTest do
   use ExUnit.Case
   doctest TodoList
-  use TodoList
 
-  test "greets the world" do
-    assert TodoList.hello() == :world
-  end
+
 
   test "creatinf a new todolist struct" do
-    
+    assert TodoList.new == %{}
+  end
+
+  test "creatigna new entry" do
+    todo_list = TodoList.new
+    entry = %{date: ~D[2020-02-02], title: "dentitst"}
+    assert TodoList.add_entry(todo_list, entry) == %{~D[2020-02-02] => [%{date: ~D[2020-02-02], title: "dentitst"}]}
   end
 end
